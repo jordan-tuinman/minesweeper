04e9be91a845c8071affab3b8066fa23a465b061
jordan-tuinman <jojotuijo@gmail.com>
2020-12-11 14:24:48 +1300

    write functions for 'counting the boom' - working

--- minesweeper.js
+++ minesweeper.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
-  cells : [
-    {
+  cells: [{
       row: 0,
       col: 0,
       isMine: false,
@@ -157,7 +156,14 @@ var board = {
 }
 
 
-function startGame () {
+function startGame() {
+  // loop board cells content
+  // call countSurroundingMines for each cell
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
+
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -166,21 +172,38 @@ function startGame () {
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
+function checkForWin() {
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
 }
 
+
+
+
+
+
+
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`: 
 //
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
+// var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+// 
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
-}
 
+function countSurroundingMines(cell) {
+  // (currently accessed cell is already specified in startgame func.) 
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+  var count = 0
+  // loop through array for values of ismine true
+  surrounding.forEach(element => {
+    if (element.isMine == true) {
+      return count ++;
+    }
+  })
+  // return total number of true mines
+return count;
+}
